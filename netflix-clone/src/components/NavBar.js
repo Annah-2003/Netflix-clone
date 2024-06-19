953dfc607af274c0ba96c22df36768c418ca115d
Annah-2003 <irene.gitau@strathmore.edu>
2024-06-19 06:01:36 -0700

    created navbar component

---
+++ netflix-clone/src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NavBar.css';
+
+function NavBar() {
+    return (
+        <div className="navbar">
+            <img className="navbar__logo" src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"alt= "Netflix Logo"/>
+            <img className="navbar__avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"alt= "User Avatar"/>
+            </div>
+    );
+    
+}
+
+export default NavBar;
